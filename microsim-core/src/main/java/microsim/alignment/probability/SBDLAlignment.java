26d18953a37a382ce36b48c23541d1f0dc031b05
Ross Richardson <r.richardson05@alumni.imperial.ac.uk>
2016-02-19 15:37:21 +0000

    Tidy up

--- microsim-core/src/main/java/microsim/alignment/probability/SBDLAlignment.java
+++ microsim-core/src/main/java/microsim/alignment/probability/SBDLAlignment.java
@@ -15,7 +15,6 @@ public class SBDLAlignment<T> extends AbstractProbabilityAlignment<T> {
 
 	@Override
 	public void align(List<T> agentList, Predicate filter, AlignmentProbabilityClosure<T> closure, double targetShare) {
-
 		if (targetShare < 0 || targetShare > 1) {
 			System.err.println("target probability must lie in [0,1]");
 			System.exit(0);
@@ -34,8 +33,7 @@ public class SBDLAlignment<T> extends AbstractProbabilityAlignment<T> {
 		for (int i=0; i<n; i++) {
 			T agent = list.get(i);
 			double p = closure.getProbability(agent);
-			double r = SimulationEngine.getRnd().nextDouble();
-			
+			double r = SimulationEngine.getRnd().nextDouble();		
 			map.put(agent, new Double(Math.log(1/r-1)+Math.log(p/(1-p))));
 		}
 		sortByComparator(map, false); // true for ascending order
