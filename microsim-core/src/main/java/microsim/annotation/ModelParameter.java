1d2db4f18ce98dfccf6f722414b179b94b3e20ca
Ross Richardson <r.richardson05@alumni.imperial.ac.uk>
2016-02-24 20:19:46 +0000

    Added @Target to ModelParameter annotation interface

--- microsim-core/src/main/java/microsim/annotation/ModelParameter.java
+++ microsim-core/src/main/java/microsim/annotation/ModelParameter.java
@@ -2,15 +2,18 @@ package microsim.annotation;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
 
 /**
- * Annotate variables of the simulation manager automatically managed by JAS gui
+ * Annotate variables of the simulation manager automatically managed by JAS-mine gui
  * to ask parameters to simulation user.
  *  
  * @author Michele Sonnessa
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
+@Target( { ElementType.FIELD })		//Added by Ross 
 public @interface ModelParameter {
 
 	public String section() default "";
