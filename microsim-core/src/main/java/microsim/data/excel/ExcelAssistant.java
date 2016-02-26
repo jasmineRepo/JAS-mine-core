a230683a1a6cbfd7d0de13c23c4e2169e41a0c08
Ross Richardson <r.richardson05@alumni.imperial.ac.uk>
2016-02-26 18:05:04 +0000

    Introduced new startLine parameter to loadCoefficientMap

--- microsim-core/src/main/java/microsim/data/excel/ExcelAssistant.java
+++ microsim-core/src/main/java/microsim/data/excel/ExcelAssistant.java
@@ -46,7 +46,28 @@ public class ExcelAssistant {
 		return MultiKeyCoefficientMap.toStringKey(val);
 	}
 	
+	/**
+	 * Load MultiKeyCoefficientMap from Excel spreadsheet data, reading from the first line of the spreadsheet
+	 * @param excelFileName: the Excel workbook (.xls) that stores the data
+	 * @param sheetName: the Excel worksheet name that stores the data
+	 * @param keyColumns: the number of columns (stored to the left of the worksheet) that represent keys. This will equal the number of keys of the MultiKeyCoefficientMap that is returned
+	 * @param valueColumns: the number of columns (stored to the right of the keys in the worksheet) that represents values, not keys.  This will equal the size of the values[] array for each MultiKey in the MultiKeyCoefficientMap
+	 * @return
+	 */
 	public static MultiKeyCoefficientMap loadCoefficientMap(String excelFileName, String sheetName, int keyColumns, int valueColumns) {
+		return loadCoefficientMap(excelFileName, sheetName, keyColumns, valueColumns, 0);		//Default start line at 0
+	}
+
+	/**
+	 * Load MultiKeyCoefficientMap from Excel spreadsheet data, choosing which line to start reading from via the startLine parameter
+	 * @param excelFileName: the Excel workbook (.xls) that stores the data
+	 * @param sheetName: the Excel worksheet name that stores the data
+	 * @param keyColumns: the number of columns (stored to the left of the worksheet) that represent keys. This will equal the number of keys of the MultiKeyCoefficientMap that is returned
+	 * @param valueColumns: the number of columns (stored to the right of the keys in the worksheet) that represents values, not keys.  This will equal the size of the values[] array for each MultiKey in the MultiKeyCoefficientMap
+	 * @param startLine: Parameter specifying the line number of which to start reading (0 is the first line)
+	 * @return
+	 */
+	public static MultiKeyCoefficientMap loadCoefficientMap(String excelFileName, String sheetName, int keyColumns, int valueColumns, int startLine) {
 		
 		MultiKeyCoefficientMap map = null;
 		
@@ -55,53 +76,45 @@ public class ExcelAssistant {
 			HSSFWorkbook workbook = new HSSFWorkbook(fileInputStream);
 			HSSFSheet worksheet = workbook.getSheet(sheetName);
 						
-			HSSFRow headerRow = worksheet.getRow(0);
+			HSSFRow headerRow = worksheet.getRow(startLine);
 			String[] keyVector = new String[keyColumns];
 			for (int j = 0; j < keyColumns; j++) {
 				HSSFCell cell = headerRow.getCell((short) j, Row.RETURN_BLANK_AS_NULL);
-				String val = getStringCellValue(cell);
-				keyVector[j] = val;
+				keyVector[j] = getStringCellValue(cell);
 			}	
 			String[] valueVector = new String[valueColumns];
 			for (int j = keyColumns; j < valueColumns + keyColumns; j++) {
 				HSSFCell cell = headerRow.getCell((short) j, Row.RETURN_BLANK_AS_NULL);
-				String val = getStringCellValue(cell);
-				valueVector[j - keyColumns] = val;
+				valueVector[j - keyColumns] = getStringCellValue(cell);
 			}
 			
 			map = new MultiKeyCoefficientMap(keyVector, valueVector);
 			
-			for (int i = 1; i <= worksheet.getLastRowNum(); i++) {
+			for (int i = startLine + 1; i <= worksheet.getLastRowNum(); i++) {
 				HSSFRow row = worksheet.getRow(i);
 				Object[] keyValueVector = null;
 				if (valueColumns == 1) {					
 					keyValueVector = new Object[keyColumns + valueColumns];
 					for (int j = 0; j < keyColumns; j++) {
 						HSSFCell cell = row.getCell((short) j, Row.RETURN_BLANK_AS_NULL);
-						Object val = getCellValue(cell);
-						keyValueVector[j] = val;
+						keyValueVector[j] = getCellValue(cell);
 					}				
 										
 					for (int j = keyColumns; j < keyColumns + valueColumns; j++) {
 						HSSFCell cell = row.getCell((short) j, Row.RETURN_BLANK_AS_NULL);
-						Object val = getCellValue(cell);
-						
-						keyValueVector[j] = val;
+						keyValueVector[j] = getCellValue(cell);						
 					}	
 				} else {
 					keyValueVector = new Object[keyColumns + 1];
 					for (int j = 0; j < keyColumns; j++) {
 						HSSFCell cell = row.getCell((short) j, Row.RETURN_BLANK_AS_NULL);
-						Object val = getCellValue(cell);
-						keyValueVector[j] = val;
+						keyValueVector[j] = getCellValue(cell);
 					}				
 					
 					Object[] values = new Object[valueColumns];
 					for (int j = 0; j < valueColumns; j++) {
 						HSSFCell cell = row.getCell((short) (j + keyColumns), Row.RETURN_BLANK_AS_NULL);
-						Object val = getCellValue(cell);
-						
-						values[j] = val;
+						values[j] = getCellValue(cell);
 					}	
 					keyValueVector[keyValueVector.length - 1] = values;
 				}
