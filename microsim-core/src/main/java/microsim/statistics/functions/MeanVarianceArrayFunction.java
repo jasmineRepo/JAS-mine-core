fdd7471143c9cd613102864d74d84a7df1dfbd8e
Ross Richardson <r.richardson05@alumni.imperial.ac.uk>
2016-02-27 12:21:19 +0000

    Changed MeanVarianceArrayFunction's Variance calculation back to population variance as it is the variance over a population of array entries.

--- microsim-core/src/main/java/microsim/statistics/functions/MeanVarianceArrayFunction.java
+++ microsim-core/src/main/java/microsim/statistics/functions/MeanVarianceArrayFunction.java
@@ -82,8 +82,7 @@ public class MeanVarianceArrayFunction extends AbstractArrayFunction implements 
 		else
 		{
 			mean = sum / (double) count;
-//			variance = (sumOfSquares - mean * sum) / (double) count;
-			variance = (sumOfSquares - mean * sum) / (double)(count - 1);		//Ross: Changed to Sample Variance to be consistent with MultiTraceFunction definition of Variance (i.e. sample not population variance) 
+			variance = (sumOfSquares - mean * sum) / (double) count; 		//This is a population variance as it is the variance of the array's data.
 		}		
 	}
 	
