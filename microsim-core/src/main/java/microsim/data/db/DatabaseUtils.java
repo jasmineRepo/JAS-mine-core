839db1ba6feb80321fa42f97d061242c0aa4e449
Ross Richardson <r.richardson05@alumni.imperial.ac.uk>
2016-03-01 19:06:02 +0000

    Corrected IllegalArgumentException text

--- microsim-core/src/main/java/microsim/data/db/DatabaseUtils.java
+++ microsim-core/src/main/java/microsim/data/db/DatabaseUtils.java
@@ -100,7 +100,7 @@ public class DatabaseUtils {
     	if (idField != null)
 			idField.setAccessible(true);
 		else throw new IllegalArgumentException("Object of type "
-				+ Object.class + " cannot be exported to .csv as it does not have a field of type PanelEntityKey.class or it is null!");
+				+ Object.class + " cannot be exported to database as it does not have a field of type PanelEntityKey.class or it is null!");
 
 		EntityTransaction tx = em.getTransaction();
 		tx.begin();
@@ -163,7 +163,7 @@ public class DatabaseUtils {
 	    	if (idField != null)
 				idField.setAccessible(true);
 			else throw new IllegalArgumentException("Object of type "
-					+ Object.class + " cannot be exported to .csv as it does not have a field of type PanelEntityKey.class or it is null!");
+					+ Object.class + " cannot be exported to database as it does not have a field of type PanelEntityKey.class or it is null!");
 
 //			final Field idField = targetCollection.iterator().next().getClass()
 //					.getDeclaredField("id");
