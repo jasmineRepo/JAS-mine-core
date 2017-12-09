62f658a4f47dd241add163d883fa0ac62026192e
re_ri <re_ri@DESKTOP-QKO1D8N.home>
2017-12-09 16:23:43 +0000

    Add weighted statistics and functions that were in algorithms
    package of LABOURsim.  Add Javadocs.

--- microsim-core/src/main/java/microsim/matching/MatchingAlgorithm.java
+++ microsim-core/src/main/java/microsim/matching/MatchingAlgorithm.java
@@ -5,10 +5,10 @@ import java.util.Comparator;
 
 import org.apache.commons.collections4.Predicate;
 
-public interface MatchingAlgorithm {
+public interface MatchingAlgorithm<T> {
 	
-	@SuppressWarnings("rawtypes")
-	public void matching(Collection collection1, Predicate filter1, Comparator comparator1, 
-			Collection collection2, Predicate filter2, MatchingScoreClosure doubleClosure, MatchingClosure matching);
+//	@SuppressWarnings("rawtypes")
+	public void matching(Collection<T> collection1, Predicate<T> filter1, Comparator<T> comparator1, 
+			Collection<T> collection2, Predicate<T> filter2, MatchingScoreClosure<T> doubleClosure, MatchingClosure<T> matching);
 	
 }
---
+++ microsim-core/src/main/java/microsim/statistics/weighted/IWeightedLongArraySource.java
@@ -0,0 +1,37 @@
+package microsim.statistics.weighted;
+
+import microsim.agent.Weight;
+
+//package microsim.statistics.weighted;
+
+/**
+ * Used by statistical object to access array of long values. 
+ *  
+ * <p>Title: JAS-mine</p>
+ * <p>Description: Java Agent-based Simulation library. Modelling in a Networked Environment</p>
+ * <p>Copyright (C) 2017 Michele Sonnessa and Ross Richardson</p>
+ *
+ * This library is free software; you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation;
+ * either version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * library; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * @author Michele Sonnessa and Ross Richardson
+ * <p>
+ */
+public interface IWeightedLongArraySource {
+	/**
+	 * Return the currently cached array of long values.
+	 * @return An array of double or a null pointer if the source is empty. 
+	 */
+	public long[] getLongArray();
+	
+	public double[] getWeights();
+}
