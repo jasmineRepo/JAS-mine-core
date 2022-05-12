61846320f7d0e73c236b6d243c499f00d1030a66
Vladimir Khodygo <vladimir.khodygo@glasgow.ac.uk>
2022-05-12 18:42:54 +0100

    UPD: redesign sidewalk alignment to use jamjam and utils interface

--- src/main/java/microsim/alignment/probability/SidewalkAlignment.java
+++ src/main/java/microsim/alignment/probability/SidewalkAlignment.java
@@ -1,50 +1,40 @@
 package microsim.alignment.probability;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
-import org.apache.commons.collections4.CollectionUtils;
+import lombok.val;
+
 import org.apache.commons.collections4.Predicate;
 
+import microsim.alignment.AlignmentUtils;
 import microsim.engine.SimulationEngine;
 
-public class SidewalkAlignment<T> extends AbstractProbabilityAlignment<T> {
+import static jamjam.Sum.cumulativeSum;
 
-	@Override
-	public void align(Collection<T> agents, Predicate<T> filter, AlignmentProbabilityClosure<T> closure, double targetShare) {
-		if (targetShare < 0. || targetShare > 1.) {
-			throw new IllegalArgumentException("target probability must lie in [0,1]");
-		}
+/**
+ * set individual probability to 1 if there is a change of the integer part of the cumulated probability, 0 otherwise
+ * @param <T>
+ */
+public class SidewalkAlignment<T> implements AlignmentUtils<T> {
+// todo docs, input validation, test, original research
+	public void align(Collection<T> agents, Predicate<T> filter, AlignmentProbabilityClosure<T> closure,
+					  double targetProbability) {
+		if (targetProbability < 0. || targetProbability > 1.)
+			throw new IllegalArgumentException("Target probability must lie in [0,1]");
 		
-		List<T> list = new ArrayList<T>();		
-		if (filter != null)
-			CollectionUtils.select(agents, filter, list);
-		else
-			list.addAll(agents);
-		
-		Collections.shuffle(list, SimulationEngine.getRnd());
-		int n = list.size();
-		double sum = 0;
-		
-		for (int i=0; i<n; i++) {
-			T agent = list.get(i);
-			
-			int oldSum = (int)sum;
-			
-			// update cumulated probability
-			sum += closure.getProbability(agent);
-			
-			// set individual probability to 1 if there is a change of the integer part of the cumulated probability, 0 otherwise
-			if ((int)sum != oldSum) { 
-				closure.align(agent, 1.0);
-			}
-			else { 
-				closure.align(agent, 0.0);
-			}
-		}
-		
+		List<T> list = extractAgentList(agents, filter);
+		if (list.size() == 0)
+			return;
+
+		Collections.shuffle(list, SimulationEngine.getRnd());// fixme does not generate 1
+
+		val cumulativeProb = cumulativeSum(list.stream().mapToDouble(closure::getProbability).toArray());
+		val cProbTruncated = IntStream.range(0, list.size()).map(i -> (int) cumulativeProb[i]).toArray();
+
+		for (var i = 0; i < list.size(); i++)
+			closure.align(list.get(i), cProbTruncated[i] != (i == 0 ? 0 : cProbTruncated[i - 1]) ? 1.0 : 0.0);
 	}
-
 }
