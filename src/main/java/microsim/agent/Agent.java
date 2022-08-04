c5fcc0f1d99ca031b939a81317776793eea70b1b
Vladimir Khodygo <vladimir.khodygo@glasgow.ac.uk>
2022-08-04 14:14:39 +0100

    MAINT, DOC: fix the agent docs, reformat the code

--- src/main/java/microsim/agent/Agent.java
+++ src/main/java/microsim/agent/Agent.java
@@ -5,14 +5,18 @@ import jakarta.persistence.Transient;
 import microsim.engine.SimulationEngine;
 import microsim.event.EventListener;
 
+/**
+ * An abstract class for objects representing agents.
+ * @param <T>
+ */
 public abstract class Agent<T> implements EventListener {
 
-	private T t;
+    private T t;
 
-	@SuppressWarnings("unchecked")
-	@Transient
-	public T getModel() {
-		t = null;
-		return (T) SimulationEngine.getInstance().getManager(t.getClass().getCanonicalName());
-	}
+    @SuppressWarnings("unchecked")
+    @Transient
+    public T getModel() {
+        t = null;
+        return (T) SimulationEngine.getInstance().getManager(t.getClass().getCanonicalName());
+    }
 }
--- src/main/java/microsim/agent/Weight.java
+++ src/main/java/microsim/agent/Weight.java
@@ -1,13 +1,10 @@
 package microsim.agent;
 
 /**
- * Interface to implement when using JAS-mine classes that depend on agents that have weights
- * 
- * @author re_ri
- *
+ * Interface to implement when using JAS-mine classes that depend on agents that have weights.
  */
 public interface Weight {
 
-	double getWeight();
-	
+    double getWeight();
+
 }
