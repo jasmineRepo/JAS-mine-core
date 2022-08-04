a01fd5c8facee6a4d164794deb22e1b7587e6e68
Vladimir Khodygo <vladimir.khodygo@glasgow.ac.uk>
2022-08-04 14:15:36 +0100

    MAINT: reformat alignment utils

--- src/main/java/microsim/alignment/AlignmentUtils.java
+++ src/main/java/microsim/alignment/AlignmentUtils.java
@@ -13,12 +13,14 @@ import java.util.List;
 public interface AlignmentUtils<T> {
     /**
      * Sorts out {@code agents} according to the filter requirements.
+     *
      * @param agents An unsorted list of agents.
      * @param filter Null, or a predicate, one for all agents - to filter some of them out.
      * @return A filtered list of agents.
      */
-    @NotNull default List<T> extractAgentList(final @NotNull Collection<T> agents,
-                                              final @Nullable Predicate<T> filter) {
+    @NotNull
+    default List<T> extractAgentList(final @NotNull Collection<T> agents,
+                                     final @Nullable Predicate<T> filter) {
         val list = new ArrayList<T>();
         if (filter != null) CollectionUtils.select(agents, filter, list);
         else list.addAll(agents);
